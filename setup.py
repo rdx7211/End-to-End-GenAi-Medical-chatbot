a96c31b20ce684a6940bd5e1a0f0601c6ab0e65f
rdx7211 <bulan4321studypurpose@gmail.com>
2025-08-19 12:15:42 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Souvik Choudhury',
+    author_email= 'souvikchoudhury582@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
